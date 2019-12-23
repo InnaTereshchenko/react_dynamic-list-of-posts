f5a22c6ae02070c1f9f634ab58b62fd2d2104fb2
Inna <inna.tereshchenko1@gmail.com>
2019-12-23 18:02:17 +0200

    Updates

---
+++ precache-manifest.385dc29b77ae243ef904f601e1720b7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d4b065029b94589df04fa2f477a48a",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "130c40d1f36b24dbaee5",
+    "url": "/react_dynamic-list-of-posts/static/css/main.6854cb0b.chunk.css"
+  },
+  {
+    "revision": "d073f7f2799935d32130",
+    "url": "/react_dynamic-list-of-posts/static/js/2.82451e1c.chunk.js"
+  },
+  {
+    "revision": "130c40d1f36b24dbaee5",
+    "url": "/react_dynamic-list-of-posts/static/js/main.a26ad141.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
